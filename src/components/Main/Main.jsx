05a44b0fec5c435a3bcb998cafb56317f458ece2
1siikaa <srishtidwivedi672@gmail.com>
2023-03-17 13:44:47 +0530

    done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,7 +8,7 @@ function Main() {
     
     <div><Header/>
     <div class='component'>
-        <h1><Body/></h1><div class='shift'>Welcome to this e-Library.<br/><h3 style={"color":"red";} >Here, You can register your profile and publish your books. You can request a book for reading reviewing purpose.</h3></div></div></div>
+        <h1><Body/></h1><div class='shift'>Welcome to this e-Library.<br/><h3>Here, You can register your profile and publish your books. You can request a book for reading reviewing purpose.</h3></div></div></div>
   )
 }
 
