88e680181f1bb7e5cda938db74ca05479192c5a9
1siikaa <srishtidwivedi672@gmail.com>
2023-03-17 13:34:04 +0530

    done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,7 +8,7 @@ function Main() {
     
     <div><Header/>
     <div class='component'>
-        <h1><Body/></h1><div class='shift'><Body/></div></div></div>
+        <h1><Body/></h1><div class='shift'>Welcome to this e-Library</div></div></div>
   )
 }
 
